8bbce340af96ea38d9731ca39bc2610f7ec4ea16
Olivier Lalonde <olalonde@gmail.com>
2012-11-26 13:58:31 +0800

    Strip out query string when checking for .json.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+var url = require('url');
+
 module.exports = function() {
   return function (req, res, next) {
-    if (req.url.match(/\.json$/)) {
+    if (url.parse(req.url).pathname.match(/\.json$/)) {
       // url ends with json
       req.headers.accept = 'application/json';
     };
